0e1aadae3ce3435acf61a81e5988aaa46bb1839f
Aravind G <aravind.gunasekaran@hpe.com>
2025-01-12 18:11:20 +0530

    Update invoice output to change "AUTHORIZED SIGNNATURE" to "AUTHORIZED SIGNATORY"

--- app.py
+++ app.py
@@ -264,14 +264,10 @@ IFSC Code: CNRB0010651"""
         self.cell(0, 10, "FOR RAMESH ENGINEERING", 0, 1, "R")
         self.cell(0, 10, "", 0, 1, "R")  # Empty line for spacing
 
-
-         # Add "AUTHORIZED SIGNNATURE" below the total
-        self.set_xy(10, rect_y + rect_height +25)
+        self.set_xy(10, rect_y + rect_height + 25)
         self.set_font("Arial", "B", 12)
-        self.cell(0, 10, "AUTHORIZED SIGNNATURE", 0, 1, "R")
+        self.cell(0, 10, "AUTHORIZED SIGNATORY", 0, 1, "R")
         self.cell(0, 10, "", 0, 1, "R")  # Empty line for spacing
-
- 
 
 
 @app.route("/")
